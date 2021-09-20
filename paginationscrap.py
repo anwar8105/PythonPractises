652397e9aaf2e57bb02abc4ce235b28425022c5e
anwar8105 <anwarrnadaf@gmail.com>
2021-09-20 15:48:30 +0530

    removed unwanted variables

--- paginationscrap.py
+++ paginationscrap.py
@@ -1,14 +1,14 @@
-# Goal collecting all books with 2 star ratings
-# from books.scrape.com
+# Goal collecting all books title's with 2 star ratings
+# sourcesite : books.scrape.com
+
 import requests, bs4
 
-# gathered_books
-gathered_books = []
+# list of collected titles
+collected_titles = []
 
-def build_books(gathered_books):
+def build_books(collected_titles):
     # base_URI - 'https://books.toscrape.com/catalogue/page-.html'
     list_URI = []
-    book_store = {}
     
     # there are total 50 pages in website
     for num in range(1,51):
@@ -19,8 +19,8 @@ def build_books(gathered_books):
         
         for data in content.select('.product_pod'):
             if data.select('.star-rating.Two'):
-                gathered_books.append(data.select('a')[1]['title'])
+                collected_titles.append(data.select('a')[1]['title'])
                 
-    return gathered_books
+    return collected_titles
         
-build_books(gathered_books)
+build_books(collected_titles)
