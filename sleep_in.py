68c5ef5b784ebb020c0853d563c18cdf8300431c
anwar8105 <anwarrnadaf@gmail.com>
2021-09-27 12:43:19 +0530

    initial commit

---
+++ sleep_in.py
@@ -0,0 +1,25 @@
+'''
+
+The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. 
+We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+
+sleep_in(False, False) → True
+sleep_in(True, False) → False
+sleep_in(False, True) → True
+'''
+
+
+def sleep_in(weekday, vacation):
+    # weekday and vacation are True, return true
+    if weekday and vacation:
+        return True
+        # weekday is false and vacation is true, return true
+    elif not weekday or vacation:
+        return True
+    # if none, return false
+    return False
+
+
+print(sleep_in(False, False))
+print(sleep_in(True, False))
+print(sleep_in(False, True))
