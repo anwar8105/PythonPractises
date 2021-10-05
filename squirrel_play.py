c5933f432b5d4b3ee08d4ba898136d2171f738b2
anwar8105 <anwarrnadaf@gmail.com>
2021-10-05 16:26:03 +0530

    initial commit

---
+++ squirrel_play.py
@@ -0,0 +1,23 @@
+# Problem Source : codingbat.com
+
+'''
+The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). 
+Unless it is summer, then the upper limit is 100 instead of 90. 
+Given an int temperature and a boolean is_summer, return True if the squirrels play and False otherwise.
+
+squirrel_play(70, False) → True
+squirrel_play(95, False) → False
+squirrel_play(95, True) → True
+'''
+
+
+def squirrel_play(temp, is_summer):
+    if is_summer:
+        return temp in range(60, 101)   # if its summer, then check if temp is between 60, 100 
+
+    return temp in range(60, 91)
+
+
+print(squirrel_play(70, False))
+print(squirrel_play(95, False))
+print(squirrel_play(95, True))
